70e3f2bcabd8fb7673aa8c85f199a9722b7dd87d
Denys Svorin <denissvorin25@gmail.com>
2025-03-11 17:49:08 +0200

    add necessary data class AlarmItem

---
+++ app/src/main/java/com/example/recycleview/presentation/details/dialogs/AlarmItem.kt
@@ -0,0 +1,9 @@
+package com.example.recycleview.presentation.details.dialogs
+
+data class AlarmItem(
+    val id: String,
+    val days: String = "",
+    val startDate: String = "",
+    val startTime: String = "",
+    val message: String = ""
+)
